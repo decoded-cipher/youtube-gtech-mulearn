d7be35b9048be416236facc60a2afe0a1ea85d12
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-03 13:59:49 +0530

    Add files via upload

---
+++ companies.js
@@ -0,0 +1,8 @@
+export const data=[
+    {
+        "siteName":"Day 1",
+        "siteURL":"https://www.youtube.com/watch?v=o-3x_9yWHVk",
+        "siteImage":"/Day1.png",
+        "siteDesc":"SunTec Business Solutions Pvt. Ltd"
+    },
+]
