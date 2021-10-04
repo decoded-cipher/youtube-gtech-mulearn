4d43b2ba22f8de9527f54b23baa22522032ff0be
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-04 11:13:07 +0530

    Update companies.js

--- companies.js
+++ companies.js
@@ -1,5 +1,11 @@
 export const data=[
     {
+        "siteName":"Day 2",
+        "siteURL":"https://www.youtube.com/watch?v=dcWeYHmMARA",
+        "siteImage":"/Day2.png",
+        "siteDesc":"Zerone Consulting Pvt. Ltd"
+    },
+    {
         "siteName":"Day 1",
         "siteURL":"https://www.youtube.com/watch?v=o-3x_9yWHVk",
         "siteImage":"/Day1.png",
