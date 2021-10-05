0b264139458b1654845505f0a56d8efdcc3c8dfb
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-06 01:04:06 +0530

    Update today.js

--- today.js
+++ today.js
@@ -1,9 +1,9 @@
 export const today = [
     {
-        "siteName": "Day 3",
-        "siteURL": "https://www.youtube.com/watch?v=HwCfe_PGXLQ",
-        "siteImage": "/Day3.png",
-        "siteDesc": "Travancore Analytics Pvt. Ltd",
-        "eventTime":"Tue Oct 05 2021 17:00:00 GMT+0530 (India Standard Time)"
+        "siteName": "Day 4",
+        "siteURL": "https://www.youtube.com",
+        "siteImage": "/Day4.png",
+        "siteDesc": "Ecesis BPO Services Pvt Ltd",
+        "eventTime":"Tue Oct 06 2021 17:00:00 GMT+0530 (India Standard Time)"
     },
 ]
