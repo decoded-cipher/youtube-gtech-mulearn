30a1f2271d4155dd3b19804fc35792eb8ae2104e
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-10-05 11:43:48 +0530

    made timer time dynamic, centre align timer test

--- today.js
+++ today.js
@@ -3,6 +3,7 @@ export const today = [
         "siteName": "Day 3",
         "siteURL": "https://www.youtube.com/watch?v=HwCfe_PGXLQ",
         "siteImage": "/Day3.png",
-        "siteDesc": "Travancore Analytics Pvt. Ltd"
+        "siteDesc": "Travancore Analytics Pvt. Ltd",
+        "eventTime":"Tue Oct 05 2021 17:00:00 GMT+0530 (India Standard Time)"
     },
 ]
