c6324402fd3631ad734f7918a33a749426475a00
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-05 11:33:34 +0530

    Add files via upload

---
+++ past.js
@@ -0,0 +1,14 @@
+export const past=[
+    {
+        "siteName":"Day 2",
+        "siteURL":"https://www.youtube.com/watch?v=dcWeYHmMARA",
+        "siteImage":"/Day2.jpg",
+        "siteDesc":"Zerone Consulting Pvt. Ltd"
+    },
+    {
+        "siteName":"Day 1",
+        "siteURL":"https://www.youtube.com/watch?v=o-3x_9yWHVk",
+        "siteImage":"/Day1.png",
+        "siteDesc":"SunTec Business Solutions Pvt. Ltd"
+    },
+]
---
+++ today.js
@@ -0,0 +1,8 @@
+export const today = [
+    {
+        "siteName": "Day 3",
+        "siteURL": "https://www.youtube.com/watch?v=HwCfe_PGXLQ",
+        "siteImage": "/Day3.png",
+        "siteDesc": "Travancore Analytics Pvt. Ltd"
+    },
+]
