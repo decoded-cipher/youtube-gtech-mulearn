a20f815144a86d9471916a4b75841ba7cd7adee1
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-06 01:01:46 +0530

    Update past.js

--- past.js
+++ past.js
@@ -1,5 +1,12 @@
 export const past=[
     {
+        "siteName": "Day 3",
+        "siteURL": "https://www.youtube.com/watch?v=HwCfe_PGXLQ",
+        "siteImage": "/Day3.png",
+        "siteDesc": "Travancore Analytics Pvt. Ltd",
+        "eventTime":"Tue Oct 05 2021 17:00:00 GMT+0530 (India Standard Time)"
+    },
+    {
         "siteName":"Day 2",
         "siteURL":"https://www.youtube.com/watch?v=dcWeYHmMARA",
         "siteImage":"/Day2.png",
