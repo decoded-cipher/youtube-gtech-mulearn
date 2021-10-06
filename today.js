6f62895e4bdc1df7d19a64800a6e6e4c455c55d4
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-06 13:52:32 +0530

    Update today.js

--- today.js
+++ today.js
@@ -1,7 +1,7 @@
 export const today = [
     {
         "siteName": "Day 4",
-        "siteURL": "https://www.youtube.com",
+        "siteURL": "https://youtu.be/KQx_2djgC_o",
         "siteImage": "/Day4.png",
         "siteDesc": "Ecesis BPO Services Pvt Ltd",
         "eventTime":"Tue Oct 06 2021 17:00:00 GMT+0530 (India Standard Time)"
