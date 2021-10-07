bb2fa52f1baac60fdfa91d7c42d21b36c50f9c6e
Akash Santhosh <33491581+akash-santhosh@users.noreply.github.com>
2021-10-07 12:21:23 +0530

    Update today.js

--- today.js
+++ today.js
@@ -4,6 +4,6 @@ export const today = [
         "siteURL": "https://youtu.be/JfJ15haSEmo",
         "siteImage": "/Day5.png",
         "siteDesc": "PIT Solutions Pvt Ltd",
-        "eventTime":"Thu Oct 07 2021 17:00:00 GMT+0530 (India Standard Time)"
+        "eventTime":"Thu Oct 07 2021 19:00:00 GMT+0530 (India Standard Time)"
     },
 ]
